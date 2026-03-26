202ea1ea53676a4d27d375a1742840e2bcc4e357
ronald617 <c5035507@hallam.shu.ac.uk>
2026-03-26 20:23:19 +0000

    WIP so far

--- app/routes/main.py
+++ app/routes/main.py
@@ -2,6 +2,19 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
+
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+@main_bp.route('/occupancy')
+def reports():
+    return render_template('occupancy.html')
+
+@main_bp.route('/food_report')
+def reports():
+    return render_template('food_report.html')
